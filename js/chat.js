db138700689f2886dd994be6cf3041e13c7cacb7
DziveQ <kacperduda592@gmail.com>
2020-02-10 19:53:54 +0100

    Modified navbar (now without <a> just JS! Added sound effect to chat.

--- js/chat.js
+++ js/chat.js
@@ -1,10 +1,10 @@
-let chatON = 0
-
 function chatOpen() {
     console.log("Chat Opened")
 
     document.getElementById("chat").style.display = "inherit";
     document.getElementById("chat-alert").style.display = "none";
+
+    document.getElementById('notify').play();
 }
 
 function closeChat() {
